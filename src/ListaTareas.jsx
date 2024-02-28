b74a99f3284d0fd6608c4f343e051d8c33ceca90
Axel Castaño <axelcastano@proton.me>
2024-02-28 07:01:36 -0300

    Añadida indicacion en ListaTareas

--- src/ListaTareas.jsx
+++ src/ListaTareas.jsx
@@ -3,10 +3,15 @@ import Tarea from './components/Tarea';
 function ListaTareas({ tareas, setTareas }) {
   return (
     <div className="w-full lg:h-full flex flex-col justify-center items-center">
-      <h1 className="text-5xl shrink-0 font-bold text-amber-400 text-center mb-8 lg:text-7xl">
+      <h1 className="text-5xl shrink-0 font-bold text-amber-400 text-center mb-8 lg:text-7xl drop-shadow">
         Tareas
       </h1>
       <div className="flex shrink-0 flex-col gap-3 items-center w-full max-w-screen-md lg:h-96 overflow-y-scroll pb-8">
+        {tareas.length === 0 && (
+          <p className="text-2xl tracking-wide opacity-60">
+            No hay tareas pendientes
+          </p>
+        )}
         {tareas.map((tarea) => (
           <Tarea
             key={tarea.id}
